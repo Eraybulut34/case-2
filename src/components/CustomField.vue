c409cd123caf2505b724219b0682fc3e3c513e0b
eraybulut34 <abdullaheraybulut@hotmail.com>
2022-01-22 20:02:21 +0300

    first load

---
+++ src/components/CustomField.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="hello">
+    <div class="tooltip">
+      {{ name }}
+      <span class="tooltiptext">{{ description }}</span>
+    </div>
+
+    <input
+      v-if="type == checkbox"
+      @change="onChange"
+      :type="type"
+      :placeholder="placeholder"
+      v-model="text"
+    />
+
+    <input
+      @change="onChange"
+      :type="type"
+      :placeholder="placeholder"
+      v-model="text"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomField",
+  methods: {
+    onChange() {
+      if (this.textchanged) {
+        this.textchanged();
+      }
+    },
+  },
+
+  computed: {
+    text: {
+      set(val) {
+        this.$emit("input", val);
+      },
+    },
+  },
+  props: {
+    textchanged: {
+      type: Function,
+    },
+    type: String,
+    name: String,
+    placeholder: String,
+    description: String,
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+.tooltip {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div v-for="d in data" :key="d.id">
+      <CustomField
+        v-model="d.default_value"
+        :type="d.input_type"
+        :name="d.name"
+        :placeholder="d.place_holder"
+        :description="d.description"
+        :id="d.id"
+      />
+    </div>
+    <button @click="postFields()">sadasdas</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import CustomField from "@/components/CustomField.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      data: null,
+      posts: null,
+      xxx: "test",
+    };
+  },
+  beforeMount() {
+    this.getFields();
+  },
+  methods: {
+    getFields() {
+      axios
+        .get("http://localhost:3000/fields")
+        .then((response) => (this.data = response.data));
+      Array.from();
+    },
+    postFields() {
+      var postData = {};
+      this.data.forEach((element) => {
+        postData["cf_" + element.id] = element.default_value;
+      });
+      axios.post("http://localhost:3000/posts", postData);
+    },
+  },
   components: {
-    HelloWorld,
+    CustomField,
   },
 };
 </script>
+
+
+<style scoped>
+* {
+  margin: 0%;
+}
+.form-action {
+  display: block;
+  justify-content: space-around;
+}
+</style>
